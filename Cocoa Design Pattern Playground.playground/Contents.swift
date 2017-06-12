4066d21c4f220374625a3728f1f376589c46692e
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-06-12 03:21:12 -0700

    mvc added

--- Cocoa Design Pattern Playground.playground/Contents.swift
+++ Cocoa Design Pattern Playground.playground/Contents.swift
@@ -21,3 +21,74 @@ let dataSource = DataSourceExample()
 tableView.dataSource = dataSource
 tableView.reloadData()
 tableView
+
+
+
+// Design Patter: Target/Action
+
+class MyTarget: NSObject {
+    func performAction() {
+        print("bullseye!")
+    }
+}
+
+let target = MyTarget()
+let button = UIButton()
+
+
+// And set our target to perform the "performAction" function when a "TouchUpInside" event occurs.
+button.addTarget(target, action: #selector(MyTarget.performAction), for: .touchUpInside)
+button.sendActions(for: .touchUpInside)
+
+
+// Design Pattern: MVC
+
+// Used to count up or down
+struct CounterModel {
+    // Only this struct can adjust the value
+    private(set) var value:Int = 0
+    
+    mutating func increment() {
+        value += 1
+    }
+    
+    mutating func decrement() {
+        value -= 1
+    }
+}
+
+
+class ViewController {
+    
+    var counterModel: CounterModel = CounterModel()
+    var counterValue: Int = 0
+    
+    let incrementButton = UIButton()
+    let decrementButton = UIButton()
+    
+    init() {
+        incrementButton.addTarget(self, action: #selector(self.didIncrement), for: .touchUpInside)
+        decrementButton.addTarget(self, action: #selector(self.didDecrement), for: .touchUpInside)
+    }
+    
+    // MARK - Controller functions
+    
+    @objc func didIncrement() {
+        counterModel.increment()
+        updateViewLayer()
+    }
+    
+    @objc func didDecrement() {
+        counterModel.decrement()
+        updateViewLayer()
+    }
+    
+    func updateViewLayer() {
+        counterValue = counterModel.value
+    }
+}
+
+let viewController = ViewController()
+viewController.incrementButton.sendActions(for: .touchUpInside)
+viewController.incrementButton.sendActions(for: .touchUpInside)
+viewController.counterValue
